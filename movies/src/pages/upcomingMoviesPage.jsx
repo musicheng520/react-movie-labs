a47b963955f22b7799acf381243713fc286dd33b
Sicheng Mu <1958775255@qq.com>
2025-10-15 11:34:12 +0100

    New button for upcoming movies page.

--- movies/src/pages/upcomingMoviesPage.jsx
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
-import { getUpcomingMovies } from "../api/tmdb-api";
-import PageTemplate from "../components/templateMovieListPage";
 import Spinner from "../components/spinner";
-import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+import PageTemplate from "../components/templateMovieListPage";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import AddToWatchlistIcon from "../components/cardIcons/addToWatchlist";
 
 const UpcomingMoviesPage = () => {
   const { data, error, isPending, isError } = useQuery({
@@ -20,17 +20,13 @@ const UpcomingMoviesPage = () => {
   }
 
   const movies = data.results;
-  const favorites = movies.filter((m) => m.favorite);
-  localStorage.setItem("favorites", JSON.stringify(favorites));
-
-  const addToFavorites = (movieId) => true;
 
   return (
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
       action={(movie) => {
-        return <AddToFavoritesIcon movie={movie} />;
+        return <AddToWatchlistIcon movie={movie} />;
       }}
     />
   );
